f0de1029caf030fc662c51e2469d22fb8ff5e16d
Square <square+sqslt@0xfc.de>
2020-06-21 19:47:38 +0200

    ad curl options

--- adBlockHostsGenerate.sh
+++ adBlockHostsGenerate.sh
@@ -25,7 +25,7 @@ LISTS='http://someonewhocares.org/hosts/hosts
 
 for list in $LISTS; do
 	echo [$(date +'%d.%m.%Y-%H:%M:%S:%N')] Loading list: $list
-	curl -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" $list >> $ADAWAYRAW
+	curl -s -S -L -A "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36" $list >> $ADAWAYRAW
 done
 
 echo [$(date +'%d.%m.%Y-%H:%M:%S:%N')] Replace 127.0.0.1 with 0.0.0.0
