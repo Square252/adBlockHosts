f2862c9d2483c9b50c91e888f16f9bba9a5eb20a
Square <square+sqslt@0xfc.de>
2020-09-19 17:47:28 +0200

    add 0.0.0.0 to removal list

--- adBlockHostsGenerate.sh
+++ adBlockHostsGenerate.sh
@@ -51,6 +51,7 @@ done
 sort -u "$ADAWAYRAW" | uniq >> "$ADAWAYCLEAN"
 
 echo ["$(date +'%d.%m.%Y-%H:%M:%S:%N')"] Removing invalid entries
+sed -i -e 's/0.0.0.0 $//' "$ADAWAYCLEAN"
 sed -i -e 's/0.0.0.0$//' "$ADAWAYCLEAN"
 
 rm "$ADAWAYRAW"
