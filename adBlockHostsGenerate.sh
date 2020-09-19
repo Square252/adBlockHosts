36d9ea90993b7846ce53eeee0236c502fba02e57
Square <square+sqslt@0xfc.de>
2020-09-19 18:01:24 +0200

    fix invalid url removal

--- adBlockHostsGenerate.sh
+++ adBlockHostsGenerate.sh
@@ -51,9 +51,9 @@ done
 sort -u "$ADAWAYRAW" | uniq >> "$ADAWAYCLEAN"
 
 echo ["$(date +'%d.%m.%Y-%H:%M:%S:%N')"] Removing invalid entries
-sed -i 's/0.0.0.0 0.0.0.0$//' "$ADAWAYCLEAN"
-sed -i 's/0.0.0.0 $//' "$ADAWAYCLEAN"
-sed -i 's/0.0.0.0$//' "$ADAWAYCLEAN"
+sed -i 's/^0.0.0.0 0.0.0.0$//' "$ADAWAYCLEAN"
+sed -i 's/^0.0.0.0 $//' "$ADAWAYCLEAN"
+sed -i 's/^0.0.0.0$//' "$ADAWAYCLEAN"
 
 rm "$ADAWAYRAW"
 
