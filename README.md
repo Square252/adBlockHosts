02ce57a5fb77466e41c65a98cfd24562e26a8a6f
square <square+sqslt@0xfc.de>
2021-09-09 01:43:31 +0200

    add new lists

--- README.md
+++ README.md
@@ -17,22 +17,24 @@ The file gets regenerated daily and is suitable for use in [PiHole](https://pi-h
 The resulting file is saved under `/var/www/html/hosts.txt`
 # Currently included source lists:
 ```
+https://adaway.org/hosts.txt
 https://blocklistproject.github.io/Lists/ads.txt
 https://blocklistproject.github.io/Lists/phishing.txt
 https://blocklistproject.github.io/Lists/tracking.txt
-https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt
-https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0
+https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
+https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
+https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
 https://raw.githubusercontent.com/mitchellkrogza/Badd-Boyz-Hosts/master/hosts
 https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts
-https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
-https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt
-https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
-https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0
-https://adaway.org/hosts.txt
-http://winhelp2002.mvps.org/hosts.txt
-http://someonewhocares.org/hosts/hosts
+https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+https://reddestdream.github.io/Projects/MinimalHosts/etc/MinimalHostsBlocker/minimalhosts
+https://someonewhocares.org/hosts/hosts
+https://winhelp2002.mvps.org/hosts.txt
+https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt'
+
 ```
--- adBlockHostsGenerate.sh
+++ adBlockHostsGenerate.sh
@@ -6,24 +6,25 @@ ADAWAYCLEAN="$(mktemp)"
 # e.g.: https://phishing.army/download/phishing_army_blocklist_extended.txt
 # e.g.: https://s3.amazonaws.com/lists.disconnect.me/simple_ad.txt
 
-LISTS='https://blocklistproject.github.io/Lists/ads.txt
+LISTS='https://adaway.org/hosts.txt
+	https://blocklistproject.github.io/Lists/ads.txt
 	https://blocklistproject.github.io/Lists/phishing.txt
 	https://blocklistproject.github.io/Lists/tracking.txt
-	https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt
-	https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+	https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0
+	https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
+	https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt
+	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
+	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts
+	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
+	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
+	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
 	https://raw.githubusercontent.com/mitchellkrogza/Badd-Boyz-Hosts/master/hosts
 	https://raw.githubusercontent.com/notracking/hosts-blocklists/master/hostnames.txt
-	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/UncheckyAds/hosts
-	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Spam/hosts
-	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Risk/hosts
-	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.Dead/hosts
-	https://raw.githubusercontent.com/FadeMind/hosts.extras/master/add.2o7Net/hosts
-	https://raw.githubusercontent.com/azet12/KADhosts/master/KADhosts.txt
-	https://raw.githubusercontent.com/AdAway/adaway.github.io/master/hosts.txt
-	https://pgl.yoyo.org/adservers/serverlist.php?hostformat=hosts&mimetype=plaintext&useip=0.0.0.0
-	https://adaway.org/hosts.txt
+	https://raw.githubusercontent.com/StevenBlack/hosts/master/hosts
+	https://reddestdream.github.io/Projects/MinimalHosts/etc/MinimalHostsBlocker/minimalhosts
+	https://someonewhocares.org/hosts/hosts
 	https://winhelp2002.mvps.org/hosts.txt
-	https://someonewhocares.org/hosts/hosts'
+	https://www.github.developerdan.com/hosts/lists/ads-and-tracking-extended.txt'
 
 for list in $LISTS; do
 	echo ["$(date +'%d.%m.%Y-%H:%M:%S:%N')"] Loading list: "$list"
